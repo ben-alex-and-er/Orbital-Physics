5d72a1ad92af91f042d2f7786f79441e61548159
Ben <ben250402@yahoo.co.uk>
2022-10-24 13:40:29 +0100

    Renamed Gravity to UnityGravity2D
    So can make new scripts for each one

--- Assets/Code/UnityGravity2D.cs
+++ Assets/Code/UnityGravity2D.cs
@@ -3,18 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gravity : MonoBehaviour
+public class UnityGravity2D : MonoBehaviour
 {
 
     public List<GameObject> gravityObjects;
+    public double initialVelocity = 0;
+    public double angle = 0; 
     private double G = 0.00000000006674;
-    private double Gtemp = 0.0006674;
+    private double Gtemp = 0.1;
     private Rigidbody2D rigidBody;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.AddForce(new Vector2((float) (initialVelocity * Math.Cos(angle)), (float) (initialVelocity * Math.Sin(angle))), ForceMode2D.Impulse);
     }
 
     // FixedUpdate is called 50 times per second
