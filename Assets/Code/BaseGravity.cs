d2f72e645cc36e98ba511c03f07defeb6b0a20f0
Ben <ben250402@yahoo.co.uk>
2023-02-17 20:55:46 +0000

    Continued from previous commit

--- Assets/Code/BaseGravity.cs
+++ Assets/Code/BaseGravity.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class BaseGravity : MonoBehaviour
 {
     protected const double G = 0.00000000006674;
+    protected const double minCollisionDistance = 2;
     protected GravityController gravityController;
     protected SystemController systemController;
 }
