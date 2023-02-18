96895c9dc959f2d0a6d3434661e4cf0b1d93d00a
Ben <ben250402@yahoo.co.uk>
2023-02-18 23:20:15 +0000

    Added limits for zooming

--- Assets/Code/CameraZoom.cs
+++ Assets/Code/CameraZoom.cs
@@ -6,11 +6,18 @@ public class CameraZoom : MonoBehaviour
     private Camera cam;
 
     [SerializeField]
-    private float zoom;
+    private float zoom = 15;
+    [SerializeField]
+    private float minField = 10;
+    [SerializeField]
+    private float maxField = 160;
 
     // Update is called once per frame
     void Update()
     {
-        cam.fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * zoom;
+        var newField = cam.fieldOfView - Input.GetAxis("Mouse ScrollWheel") * zoom;
+
+        if (newField < maxField && newField > minField)
+            cam.fieldOfView = newField;
     }
 }
