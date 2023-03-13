a83891547dca168ed1f7c13982d7a7058cdea1ff
Ben <ben250402@yahoo.co.uk>
2023-03-13 17:05:12 +0000

    New 3D planets can now be clicked on
    And change Planet Editor

--- Assets/Code/3D/ClickObject3D.cs
+++ Assets/Code/3D/ClickObject3D.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class ClickObject3D : MonoBehaviour
 {
     [SerializeField]
-    private PlanetEditor3D planetEditor;
-
-    [SerializeField]
     private Object3D selfObject;
 
+    private PlanetEditor3D planetEditor;
+
+    private void Start()
+    {
+        planetEditor = FindObjectOfType<PlanetEditor3D>();
+    }
     public void OnMouseDown()
     {
         planetEditor.SetPlanet(selfObject, false);
